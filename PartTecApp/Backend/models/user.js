ad96f5b3359fec560cd2fa42b45d2bd05af0e8ab
Mo963syr <moafaqaqeed01@gmail.com>
2025-07-15 11:31:58 +0300

    coom

--- PartTecApp/Backend/models/user.js
+++ PartTecApp/Backend/models/user.js
@@ -4,25 +4,25 @@ const carModel = require('./carModel');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'يجب إدخال الاسم']
+    required: [true, 'يجب إدخال الاسم'],
   },
   email: {
     type: String,
     required: [true, 'يجب إدخال البريد الإلكتروني'],
     unique: true,
     lowercase: true,
-    match: [/\S+@\S+\.\S+/, 'بريد إلكتروني غير صالح']
+    match: [/\S+@\S+\.\S+/, 'بريد إلكتروني غير صالح'],
   },
   password: {
     type: String,
     required: [true, 'يجب إدخال كلمة المرور'],
-    minlength: [6, 'يجب أن تكون كلمة المرور على الأقل 6 أحرف']
+    minlength: [6, 'يجب أن تكون كلمة المرور على الأقل 6 أحرف'],
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
-  ,carModel
+    default: Date.now,
+  },
+  carModel,
 });
 
 module.exports = mongoose.model('User', userSchema);
