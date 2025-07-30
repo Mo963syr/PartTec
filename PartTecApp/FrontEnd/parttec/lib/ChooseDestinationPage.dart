68ccb87827b7b6336db2dbd58c52e28989093afb
Mo963syr <moafaqaqeed01@gmail.com>
2025-07-30 13:01:39 +0300

    cart

---
+++ PartTecApp/FrontEnd/parttec/lib/ChooseDestinationPage.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'package:parttec/groupedorderspage.dart'; // تأكد من وجود هذا الملف
+
+class ChooseDestinationPage extends StatelessWidget {
+  const ChooseDestinationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('اختر الصفحة'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
+              icon: Icon(Icons.home),
+              label: Text('الذهاب إلى الصفحة الرئيسية'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => HomePage()),
+                );
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton.icon(
+              icon: Icon(Icons.list_alt),
+              label: Text('عرض الطلبات المجمعة'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => GroupedOrdersPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
